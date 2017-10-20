f86f1fa27cd6c7a720ae6cd946b27efa80a5c003
KokovDanil <email>
2017-10-21 02:24:54 +0300

    stop words

--- src/main/java/com/example/DeleteStopWords.java
+++ src/main/java/com/example/DeleteStopWords.java
@@ -1,17 +1,20 @@
 package com.example;
 
-/**
- * Created by Данил on 21.10.2017.
- */
+
 public class DeleteStopWords {
-    public static String[] stopWords = {"это", "как", "так", "и", "в", "над", "к", "до", "не", "на", "но", "за", "то",
-                    "с", "ли", "а", "во", "от", "со", "для", "о", "же", "ну", "вы", "бы", "что", "кто", "он", "она"};
+    public static String[] stopWords = {"это ", "как ", "так ", "и ", "в ", "над ", "к ", "до ", "не ", "на ", "но ", "за ", "то ",
+                    "с ", "ли ", "а ", "во ", "от ", "со ", "для ", "о ", "же ", "ну ", "вы ", "бы ", "что ", "кто ", "он ", "она "};
+
 
     public String deleteStopWords (String str){
-        for (int i = 0; i < stopWords.length; i++){
-            str.replaceAll(stopWords[i] ,"");
-        }
 
+        str = str.toLowerCase();
+
+        str = str.replaceAll("[^a-zA-Zа-яА-Я]", " ");
+        for (String i: stopWords) {
+            str = str.replaceAll(i, "");
+        }
+        System.out.println(str);
         return str;
     }
 }
