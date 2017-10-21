bea643eef33a03407f3a6a06bc59f7b9e08623ca
KokovDanil <email>
2017-10-21 08:41:15 +0300

    Fixed CosCompar.

--- src/main/java/com/example/CosineComparator.java
+++ src/main/java/com/example/CosineComparator.java
@@ -9,8 +9,8 @@ import java.util.stream.Collectors;
 class CosineComparator {
     private static CosineSimilarity sim = new CosineSimilarity();
     public double compareNews(String inputNews, String foundNews) {
-        Map<CharSequence, Integer> leftVector = Arrays.stream(inputNews.split("")).collect(Collectors.toMap(c -> c, c -> 1, Integer::sum));
-        Map<CharSequence, Integer> rightVector = Arrays.stream(foundNews.split("")).collect(Collectors.toMap(c -> c, c -> 1, Integer::sum));
+        Map<CharSequence, Integer> leftVector = Arrays.stream(inputNews.split(" ")).collect(Collectors.toMap(c -> c, c -> 1, Integer::sum));
+        Map<CharSequence, Integer> rightVector = Arrays.stream(foundNews.split(" ")).collect(Collectors.toMap(c -> c, c -> 1, Integer::sum));
         return sim.cosineSimilarity(leftVector, rightVector);
     }
 }
